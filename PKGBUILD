5ba4bc291886f191e259cdd4fa702b9ac8fc14dd
Anurag Roy <anuragr9847@gmail.com>
2022-01-14 16:19:53 +0530

    Initial commit

---
+++ PKGBUILD
@@ -0,0 +1,31 @@
+# Maintainer: Anurag Roy <anuragr9847@gmail.com>
+_pkgname="dmenu"
+pkgname="$_pkgname-royarg-git"
+pkgver=5.1.r2.bd73af3
+pkgrel=1
+pkgdesc="A modified version of the dynamic menu for X, originally designed for dwm."
+arch=('i686' 'x86_64')
+url="https://github.com/RoyARG02/$_pkgname"
+license=('MIT')
+depends=('sh' 'libxinerama' 'libxft')
+makedepends=('git')
+provides=("$_pkgname")
+conflicts=("$_pkgname" "$_pkgname-git")
+source=("git+$url.git")
+md5sums=('SKIP')
+
+pkgver() {
+	cd "$_pkgname"
+	git describe --long --tags | sed 's/\([^-]*-\)g/r\1/;s/-/./g'
+}
+
+build() {
+	cd "$_pkgname"
+	make
+}
+
+package() {
+	cd "$_pkgname"
+	make PREFIX=/usr DESTDIR="$pkgdir/" install
+  install -Dm644 LICENSE "$pkgdir"/usr/share/licenses/$_pkgname/LICENSE
+}
---
+++ pre-commit.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/sh
+
+# Pre-commit script
+# - Generate the checksums in the PKGBUILD file and add it to the commit if
+# needed
+# - Generate the .SRCINFO file and add it to the commit if needed
+
+# Cleanup function
+cleanup() {
+  # Remove directories and files created/downloaded by makepkg
+  rm --force --recursive pkg src *.part *.pkg.tar.zst *.tar.gz *.desktop *.1 LICENSE
+}
+
+trap cleanup SIGINT EXIT
+
+# Generate the checksums in the PKGBUILD file
+updpkgsums
+
+# Remove files downloaded by updpkgsums
+rm *.tar.gz *.desktop *.1 LICENSE
+
+# Check if the checksums in the PKGBUILD file changed
+if [ -n "$(git diff PKGBUILD | grep '^+md5sums')" ]; then
+  printf "Updated the checksums in the PKGBUILD file\n"
+  git add PKGBUILD
+  printf "Added the PKGBUILD file to the commit\n"
+fi
+
+# Generate the .SRCINFO file based on the PKGBUILD file
+makepkg --printsrcinfo > .SRCINFO
+
+# Check if the .SRCINFO file was created/changed
+if [ -n "$(git status --short | grep ' .SRCINFO$')" ]; then
+  printf "Updated the .SRCINFO file\n"
+  git add .SRCINFO
+  printf "Added the .SRCINFO file to the commit\n"
+fi
+
+printf "All done!\n"
